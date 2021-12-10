1b15bee91a0b22900df929306d38d0fd069f442f
jongwookim2019 <rlawhddn77@naver.com>
2021-12-10 11:28:26 +0900

    test

--- seleniumTest.py
+++ seleniumTest.py
@@ -1,6 +1,7 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome import options
 
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -8,11 +9,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
+#크롬 에러 수정
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enavle-logging'])
+driver = webdriver.Chrome(options=options)
 
 
 #설정된 Driver Web 주소 불러오기
 #주소 설정
-URL = 'https://www.naver.com'
+URL = 'https://nid.naver.com/nidlogin.login?url=http%3A%2F%2Fmail.naver.com%2F'
 #드라이버 로드(chromedriver)
 driver = webdriver.Chrome(executable_path='chromedriver')
 #담아준 주소를 브라우저에서 띄움
@@ -21,5 +26,13 @@ driver.get(url=URL)
 # 현재 url 얻기
 print(driver.current_url)
 
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME,'btn_text'))
+    )
+finally:
+    driver.quit()
+    print(EC.presence_of_element_located((By.CLASS_NAME,'btn_text')))
+
 #브라우저 닫기
-driver.close()
+#driver.close()
